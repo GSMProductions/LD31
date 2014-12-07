853de8195d1384826cf7bab351615d6539789713
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2014-12-07 17:28:57 +0100

    Added BGM for menu screen along with transitions

---
+++ Dev/Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioSource[] bgm_tracks;
+    private int currentMusic = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetMusic(int index) {
+        bgm_tracks[currentMusic].Stop();
+        bgm_tracks[index].Play();
+        currentMusic = index;
+    }
+}
